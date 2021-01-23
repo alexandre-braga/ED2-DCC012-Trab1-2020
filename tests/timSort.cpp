1e0ace83aafc005018dffc7463fd1301d8605ccc
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-01-23 14:33:40 -0300

    VerificaFimRun e faz o InsertionSort nas Runs

--- tests/timSort.cpp
+++ tests/timSort.cpp
@@ -2,9 +2,29 @@
 #include <fstream>
 #include <vector>
 #include <stack>
+
+#include "../include/Registro.hpp"
  
 const int MIN_RUN = 64;
 
+/*Merge Sort:*/
+
+
+/*Insertion Sort: */
+void insertionSort(int vet[], int inicio, int fim) 
+{ 
+    for (int i = inicio + 1; i <= fim; i++) 
+    { 
+        int pivo = vet[i]; 
+        int j = i - 1; 
+        while (j >= inicio && vet[j] > pivo) 
+        { 
+            vet[j+1] = vet[j]; 
+            j--; 
+        } 
+        vet[j+1] = pivo; 
+    } 
+} 
 
 /*Mantém a runs em tamanho crescente enquanto desce a pilha:*/
 enquanto (o tamanho das runs >=2){
@@ -28,20 +48,49 @@ while (pilhaDeRuns.size >= 2){
     }
     
 }
-        Começando a partir da posição do elemento atual, procure o Run (um sub-vetor ordenado) no vetor de entrada. Por definição o Run será pelo menos o elemento atual e o próximo (pois formará um vetor ordenado, seja crescente ou decrescente), sendo que a composição de mais elementos no Run dependerá da forma como os elementos estão organizados. O próximo elemento é considerado se ao considerá-lo no Run atual, o Run continue ordenado. Se o Run final está ordenado de forma decrescente, os elementos são "reordenados" em uma ordem crescente (por meio de um algoritmo  simples de inversão de vetor).
 
-void verificaRun(, int a, int b){
+
+/*Começando a partir da posição do elemento atual, procure o Run (um sub-vetor ordenado) no vetor de entrada. Por definição o Run será pelo menos o elemento atual e o próximo (pois formará um vetor ordenado, seja crescente ou decrescente), sendo que a composição de mais elementos no Run dependerá da forma como os elementos estão organizados. O próximo elemento é considerado se ao considerá-lo no Run atual, o Run continue ordenado. Se o Run final está ordenado de forma decrescente, os elementos são "reordenados" em uma ordem crescente (por meio de um algoritmo  simples de inversão de vetor).
+-wikipedia
+*/
+
+void verificaFimRun(int vet[], int i){
+    int fimrum = 0;
+    if(vet[i+1]>=vet[i])
+        while(vet[i]<=vet[i+1]){
+            i++
+            if(vet[i] == NULL){
+                return fimrum;
+            }
+            fimrun++;
+        }
+        if(fimrum < MIN_RUN)
+            fimrun+=MIN_RUN-fimrum;
+        return fimrum;
+    else
+        while(vet[i]>=vet[i+1]){
+           i++
+           if(vet[i] == NULL){
+                return fimrum;
+            }
+           fimrun++;
+        }
+        std::reverse(vet.a, vet.fimrun);
+        if(fimrum < MIN_RUN)
+           fimrum+=MIN_RUN-fimrum;
+        return fimrum;
 }
 
-void timSort(){
-    /*Organiza as RUNS:*/
-    stack<int> pilhaDeRuns;
-    n = vet.size;
-    for (int i = 0; i < n; i+=RUN) 
-        insertionSort(vet, i, min((i+RUN-1),(n-1)));
-        //Insere na pilha os indices das runs
-        pilhaDeRuns.push(i);
+
+
+void timSort(vector<Registro>& vet, int(*comp)(const Registro&, const Registro&)){
+    /*Organiza as RUNS e faz o InsertionSort nelas:*/
+    for (int i = 0, int j = verificaFimRun(vet,i); i < n; i+=j){
+        insertionSort(vet, i, j);
+    }
+
     /*Faz o Merge para as RUNS já em ordem crescente de tamanho*/
+    n = vet.size;
     int RUNAux = MIN_RUN;
     while RUNAux < n{
         for(i = 0; i < n; i+=2*RUNAux){
@@ -58,4 +107,5 @@ while RUNAux < tamArray
     um for (com i no inicio do vetor; i menor que o tamanho do vetor; e tendo como passo duas RUN) 
         array[i até i+RUNAux] = Merge(array[i até i+RUNAux], array[i+RUNAux até i+2*RUNAux] )
     RUNAux = 2*RUNAux
+
   
