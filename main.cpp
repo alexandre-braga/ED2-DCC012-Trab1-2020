3daeac9225d65bec5aa1cde74b19087c8c8fdb32
caiocrocha <caiocedrola@ice.ufjf.br>
2021-01-23 20:36:06 -0300

    Ordena e escreve arquivo

--- main.cpp
+++ main.cpp
@@ -9,16 +9,38 @@
 #define ERR_NENHUM_ARG 1
 #define ERR_FALHA_ARQ  2
 
-void calculaTotalDiarios(vector<Registro>& lista)
+void calculaTotalDiarios(vector<Registro>& arr)
 {
-	std::vector<Registro>::reverse_iterator it = lista.rbegin();
+	std::vector<Registro>::reverse_iterator it = arr.rbegin();
 	std::vector<Registro>::reverse_iterator next;
 
-	while ((next = it + 1) != lista.rend()) {
+	while ((next = it + 1) != arr.rend()) {
 		if (it->code() == next->code()) {
-			it->setCases(it->cases() - next->cases());
+			int c = it->cases() - next->cases();
+			int d = it->deaths() - next->deaths();
+			if(c >= 0) 
+				it->setCases(c);
+			else
+				it->setCases(0);
+			if(d >= 0) 
+				it->setDeaths(d);
+			else
+				it->setDeaths(0);
 		}
 		it = next;
+	}
+}
+
+void escrevePreProcessado(vector<Registro>& arr, ofstream& file)
+{
+	file << "date,state,name,code,cases,deaths" << std::endl;
+	for (auto it = arr.begin(); it != arr.end(); ++it) {
+		file << it->date()   << ",";
+		file << it->state()  << ","; 
+		file << it->city()   << ",";
+		file << it->code()   << ",";
+		file << it->cases()  << ",";
+		file << it->deaths() << std::endl;
 	}
 }
 
@@ -32,31 +54,39 @@ int main(int argc, char *argv[])
 	}
 
 	while (--argc) {
-		std::ifstream f;
+		std::ifstream fin;
 
-		f.open(*++argv);
+		fin.open(*++argv);
 
-		if (!f.is_open()) {
+		if (!fin.is_open()) {
 			std::cerr << progname << ": falha ao abrir o arquivo `" << *argv << "`\n";
 			exit(ERR_FALHA_ARQ);
 		}
 
-		f.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 		Registro registro;
 		std::vector<Registro> arrayRegistros;
 
-		while (f >> registro) {
+		while (fin >> registro) {
 			arrayRegistros.push_back(registro);
 		}
+
+		fin.close();
+
 		heapSort(arrayRegistros, Registro::comparaLocalData);
 		calculaTotalDiarios(arrayRegistros);
 
-		for (const Registro& registro : arrayRegistros) {
-			std::cout << registro << std::endl;
+		std::ofstream fout;
+		fout.open("brazil_covid19_cities_processado.csv");
+		if(!fout.fail()) {
+			escrevePreProcessado(arrayRegistros, fout);
 		}
-
-		f.close();
+		else {
+			std::cout << progname << ": falha ao escrever arquivo\n";
+		}
+		fout.close();
 	}
+
 	return 0;
 }
